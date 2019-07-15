7a4847cbc80192bfd343c5895edbbed51a249895
LTR <2840743882@qq.com>
2019-07-15 21:33:36 +0800

    testjava添加方法test()

--- src/main/java/com/itheima/git_test_project/TestJava.java
+++ src/main/java/com/itheima/git_test_project/TestJava.java
@@ -4,4 +4,9 @@ public class TestJava {
     public static void main(String[] args){
         System.out.println("00000000000");
     }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
